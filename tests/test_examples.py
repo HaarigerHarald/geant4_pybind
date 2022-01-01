09c014010d1a5fc0bc5152d13d85f4cffd75d676
HaarigerHarald <haarigerharald@gmx.at>
2022-01-01 23:14:15 +0100

    Add g04 and exampleB3b to the tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,18 +6,16 @@ import subprocess
 def test_example(dir, py_file, macro_file, out_file, extra_params=[]):
     examples_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../examples')
 
-    py_file = os.path.join(os.path.join(examples_dir, dir), py_file)
-    macro_file = os.path.join(os.path.join(examples_dir, dir), macro_file)
-    out_file = os.path.join(os.path.join(examples_dir, dir), out_file)
-
     params = [sys.executable, py_file]
     params += extra_params
     params += [macro_file]
-    output = subprocess.check_output(params)
+    output = subprocess.check_output(params, cwd=os.path.join(examples_dir, dir))
 
-    with open(out_file, 'r') as file:
-        if output.decode('utf-8').find(file.read()) == -1:
-            raise Exception(output.decode('utf-8'))
+    if out_file != False:
+        out_file = os.path.join(os.path.join(examples_dir, dir), out_file)
+        with open(out_file, 'r') as file:
+            if output.decode('utf-8').find(file.read()) == -1:
+                raise Exception(output.decode('utf-8'))
 
 
 os.environ['G4FORCE_RUN_MANAGER_TYPE'] = 'Serial'
@@ -28,7 +26,7 @@ test_example('B2', 'exampleB2a.py', 'exampleB2.in', 'exampleB2a.out')
 test_example('B2', 'exampleB2b.py', 'exampleB2.in', 'exampleB2b.out')
 
 test_example('B3', 'exampleB3a.py', 'exampleB3.in', 'exampleB3a.out')
-#test_example('B3', 'exampleB3b.py', 'exampleB3.in', 'exampleB3b.out')
+test_example('B3', 'exampleB3b.py', 'exampleB3.in', False)
 
 test_example('B4', 'exampleB4a.py', 'exampleB4.in', 'exampleB4a.out', ['-m'])
 test_example('B4', 'exampleB4b.py', 'exampleB4.in', 'exampleB4b.out', ['-m'])
@@ -36,3 +34,5 @@ test_example('B4', 'exampleB4c.py', 'exampleB4.in', 'exampleB4c.out', ['-m'])
 test_example('B4', 'exampleB4d.py', 'exampleB4.in', 'exampleB4d.out', ['-m'])
 
 test_example('B5', 'exampleB5.py', 'exampleB5.in', 'exampleB5.out')
+
+test_example('gdml', 'g04.py', 'g04.mac', False, ['auxiliary.gdml'])
