51a590b6e091875002d1ba3a8529824dfff551de
HaarigerHarald <haarigerharald@gmx.at>
2022-08-26 21:20:29 +0200

    Move stub generation flag

--- setup.py
+++ setup.py
@@ -20,9 +20,6 @@ class CMakeBuild(build_ext):
         import importlib.util
         import pybind11_stubgen as stubgen
 
-        global AUTO_STUB_GENERATION
-        AUTO_STUB_GENERATION = True
-
         extdir = os.path.dirname(os.path.realpath(self.get_ext_fullpath(ext.name)))
 
         spec = importlib.util.spec_from_file_location(
@@ -101,6 +98,8 @@ class CMakeBuild(build_ext):
 
         self.generate_stubs(ext)
 
+
+AUTO_STUB_GENERATION = True
 
 with open("README.md", "r") as readme:
     long_desc = readme.read()
