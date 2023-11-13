e8ca4a6cd7ec798c6cdf28bc5d944005fb125737
HaarigerHarald <haarigerharald@gmx.at>
2023-11-13 17:39:15 +0100

    Provide trampoline class for custom random engine

--- source/global/pyRandomEngines.cc
+++ source/global/pyRandomEngines.cc
@@ -20,21 +20,85 @@ namespace py = pybind11;
 
 using namespace CLHEP;
 
+class PyHepRandomEngine : public HepRandomEngine, public py::trampoline_self_life_support {
+public:
+   using HepRandomEngine::HepRandomEngine;
+
+   double flat() override { PYBIND11_OVERRIDE_PURE(double, HepRandomEngine, flat); }
+
+   void flatArray(const int size, double *vect) override
+   {
+      py::function flatArray = py::get_override(static_cast<const HepRandomEngine *>(this), "flatArray");
+
+      if (flatArray) {
+         py::list pVect(size);
+         flatArray(pVect);
+
+         for (size_t i = 0; i < pVect.size(); i++) {
+            vect[i] = pVect[i].cast<double>();
+         }
+      } else {
+         py::pybind11_fail("Tried to call pure virtual function \"HepRandomEngine::flatArray\"");
+      }
+   }
+
+   void setSeed(long seed, int aux) override { PYBIND11_OVERRIDE_PURE(void, HepRandomEngine, setSeed, seed, aux); }
+
+   void setSeeds(const long *seeds, int length) override
+   {
+      PYBIND11_OVERRIDE_PURE(void, HepRandomEngine, setSeeds, std::vector<long>(seeds, seeds + length));
+   }
+
+   void saveStatus(const char filename[] = "Config.conf") const override
+   {
+      PYBIND11_OVERRIDE_PURE(void, HepRandomEngine, saveStatus, filename);
+   };
+
+   void restoreStatus(const char filename[] = "Config.conf") override
+   {
+      PYBIND11_OVERRIDE_PURE(void, HepRandomEngine, restoreStatus, filename);
+   }
+
+   void showStatus() const override { PYBIND11_OVERRIDE_PURE(void, HepRandomEngine, showStatus); };
+
+   std::string name() const override { PYBIND11_OVERRIDE_PURE(std::string, HepRandomEngine, name); }
+
+   // virtual std::ostream &put(std::ostream &os) const;
+   // virtual std::istream &get(std::istream &is);
+
+   // virtual std::istream &getState(std::istream &is);
+
+   std::vector<unsigned long> put() const override
+   {
+      PYBIND11_OVERRIDE(std::vector<unsigned long>, HepRandomEngine, put);
+   }
+
+   bool get(const std::vector<unsigned long> &v) override { PYBIND11_OVERRIDE(bool, HepRandomEngine, get, v); }
+
+   bool getState(const std::vector<unsigned long> &v) override
+   {
+      PYBIND11_OVERRIDE(bool, HepRandomEngine, getState, v);
+   }
+};
+
 void export_RandomEngines(py::module &m)
 {
-   py::class_<HepRandomEngine>(m, "HepRandomEngine", "base class of random engine")
+   py::class_<HepRandomEngine, PyHepRandomEngine>(m, "HepRandomEngine", "base class of random engine")
 
       .def("flat", &HepRandomEngine::flat)
       .def("flatArray",
            [](HepRandomEngine &self, py::list &vect) {
+              double dVect[vect.size()];
+              self.flatArray(vect.size(), dVect);
+
               for (size_t i = 0; i < vect.size(); i++) {
-                 vect[i] = self.flat();
+                 vect[i] = dVect[i];
               }
            })
 
       .def("setSeed", &HepRandomEngine::setSeed, py::arg("seed"), py::arg("aux"))
       .def("setSeeds",
-           [](HepRandomEngine &self, const std::vector<long> &seeds, int x) { self.setSeeds(seeds.data(), x); })
+           [](HepRandomEngine &self, const std::vector<long> &seeds) { self.setSeeds(seeds.data(), seeds.size()); })
 
       .def("saveStatus", &HepRandomEngine::saveStatus, py::arg("filename") = "Config.conf")
       .def("restoreStatus", &HepRandomEngine::restoreStatus, py::arg("filename") = "Config.conf")
