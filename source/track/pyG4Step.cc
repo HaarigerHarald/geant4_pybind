54a420faf3dc378e1594d8988f3f8fc947231ac1
HaarigerHarald <haarigerharald@gmx.at>
2024-01-17 13:49:39 +0100

    Add function bindings for G4Track and G4Step

--- source/track/pyG4Step.cc
+++ source/track/pyG4Step.cc
@@ -2,6 +2,35 @@
 #include <pybind11/stl.h>
 
 #include <G4Step.hh>
+#include <G4GeomSplitter.hh>
+#include <G4VPVParameterisation.hh>
+#include <G4VSolid.hh>
+#include <G4NavigationHistory.hh>
+#include <G4ReferenceCountedHandle.hh>
+#include <G4DensityEffectData.hh>
+#include <G4DensityEffectCalculator.hh>
+#include <G4ProductionCuts.hh>
+#include <G4VUserRegionInformation.hh>
+#include <G4MaterialCutsCouple.hh>
+#include <G4UserLimits.hh>
+#include <G4FieldManager.hh>
+#include <G4FastSimulationManager.hh>
+#include <G4UserSteppingAction.hh>
+#include <G4VSensitiveDetector.hh>
+#include <G4SmartVoxelHeader.hh>
+#include <G4VisAttributes.hh>
+#include <G4VProcess.hh>
+#include <G4Run.hh>
+#include <G4Event.hh>
+#include <G4Polyline.hh>
+#include <G4ProcessManager.hh>
+#include <G4VTrackingManager.hh>
+#include <G4DecayTable.hh>
+#include <G4ParticleTable.hh>
+#include <G4ParticlePropertyTable.hh>
+#include <G4PrimaryParticle.hh>
+#include <G4DecayProducts.hh>
+#include <G4VAuxiliaryTrackInformation.hh>
 
 #include "typecast.hh"
 #include "opaques.hh"
@@ -10,17 +39,51 @@ namespace py = pybind11;
 
 void export_G4Step(py::module &m)
 {
-   py::class_<G4Step>(m, "G4Step", "step class")
-      .def(py::init<>())
-      .def(py::init<const G4Step &>())
+   py::class_<G4Step>(m, "G4Step")
 
-      .def("GetTrack", &G4Step::GetTrack, py::return_value_policy::reference)
-      .def("GetPreStepPoint", &G4Step::GetPreStepPoint, py::return_value_policy::reference_internal)
-      .def("GetPostStepPoint", &G4Step::GetPostStepPoint, py::return_value_policy::reference_internal)
-      .def("GetTotalEnergyDeposit", &G4Step::GetTotalEnergyDeposit)
-      .def("GetStepLength", &G4Step::GetStepLength)
+      .def(py::init<>())
+      .def("__copy__", [](const G4Step &self) { return new G4Step(self); })
+      .def("__deepcopy__", [](const G4Step &self, py::dict) { return new G4Step(self); })
+      .def("AddNonIonizingEnergyDeposit", &G4Step::AddNonIonizingEnergyDeposit, py::arg("value"))
+      .def("AddTotalEnergyDeposit", &G4Step::AddTotalEnergyDeposit, py::arg("value"))
+      .def("ClearFirstStepFlag", &G4Step::ClearFirstStepFlag)
+      .def("ClearLastStepFlag", &G4Step::ClearLastStepFlag)
+      .def("CopyPostToPreStepPoint", &G4Step::CopyPostToPreStepPoint)
+      .def("DeleteSecondaryVector", &G4Step::DeleteSecondaryVector)
+      .def("GetControlFlag", &G4Step::GetControlFlag)
+      .def("GetDeltaEnergy", &G4Step::GetDeltaEnergy)
+      .def("GetDeltaMomentum", &G4Step::GetDeltaMomentum)
       .def("GetDeltaPosition", &G4Step::GetDeltaPosition)
       .def("GetDeltaTime", &G4Step::GetDeltaTime)
-      .def("GetDeltaMomentum", &G4Step::GetDeltaMomentum)
-      .def("GetDeltaEnergy", &G4Step::GetDeltaEnergy);
+      .def("GetNonIonizingEnergyDeposit", &G4Step::GetNonIonizingEnergyDeposit)
+      .def("GetNumberOfSecondariesInCurrentStep", &G4Step::GetNumberOfSecondariesInCurrentStep)
+      .def("GetPointerToVectorOfAuxiliaryPoints", &G4Step::GetPointerToVectorOfAuxiliaryPoints,
+           py::return_value_policy::reference)
+
+      .def("GetPostStepPoint", &G4Step::GetPostStepPoint, py::return_value_policy::reference)
+      .def("GetPreStepPoint", &G4Step::GetPreStepPoint, py::return_value_policy::reference)
+      .def("GetSecondary", &G4Step::GetSecondary, py::return_value_policy::reference)
+      .def("GetSecondaryInCurrentStep", &G4Step::GetSecondaryInCurrentStep, py::return_value_policy::reference)
+      .def("GetStepLength", &G4Step::GetStepLength)
+      .def("GetTotalEnergyDeposit", &G4Step::GetTotalEnergyDeposit)
+      .def("GetTrack", &G4Step::GetTrack, py::return_value_policy::reference)
+      .def("GetfSecondary", &G4Step::GetfSecondary, py::return_value_policy::reference)
+      .def("InitializeStep", &G4Step::InitializeStep, py::arg("aValue"))
+      .def("IsFirstStepInVolume", &G4Step::IsFirstStepInVolume)
+      .def("IsLastStepInVolume", &G4Step::IsLastStepInVolume)
+      .def("NewSecondaryVector", &G4Step::NewSecondaryVector, py::return_value_policy::reference)
+      .def("ResetNonIonizingEnergyDeposit", &G4Step::ResetNonIonizingEnergyDeposit)
+      .def("ResetTotalEnergyDeposit", &G4Step::ResetTotalEnergyDeposit)
+      .def("SetControlFlag", &G4Step::SetControlFlag, py::arg("StepControlFlag"))
+      .def("SetFirstStepFlag", &G4Step::SetFirstStepFlag)
+      .def("SetLastStepFlag", &G4Step::SetLastStepFlag)
+      .def("SetNonIonizingEnergyDeposit", &G4Step::SetNonIonizingEnergyDeposit, py::arg("value"))
+      .def("SetPointerToVectorOfAuxiliaryPoints", &G4Step::SetPointerToVectorOfAuxiliaryPoints, py::arg("vec"))
+      .def("SetPostStepPoint", &G4Step::SetPostStepPoint, py::arg("value"))
+      .def("SetPreStepPoint", &G4Step::SetPreStepPoint, py::arg("value"))
+      .def("SetSecondary", &G4Step::SetSecondary, py::arg("value"))
+      .def("SetStepLength", &G4Step::SetStepLength, py::arg("value"))
+      .def("SetTotalEnergyDeposit", &G4Step::SetTotalEnergyDeposit, py::arg("value"))
+      .def("SetTrack", &G4Step::SetTrack, py::arg("value"))
+      .def("UpdateTrack", &G4Step::UpdateTrack);
 }
--- source/track/pyG4Track.cc
+++ source/track/pyG4Track.cc
@@ -2,6 +2,33 @@
 #include <pybind11/stl.h>
 
 #include <G4Track.hh>
+#include <G4GeomSplitter.hh>
+#include <G4ProductionCuts.hh>
+#include <G4VUserRegionInformation.hh>
+#include <G4MaterialCutsCouple.hh>
+#include <G4UserLimits.hh>
+#include <G4FieldManager.hh>
+#include <G4FastSimulationManager.hh>
+#include <G4UserSteppingAction.hh>
+#include <G4VPVParameterisation.hh>
+#include <G4VSensitiveDetector.hh>
+#include <G4VSolid.hh>
+#include <G4SmartVoxelHeader.hh>
+#include <G4VisAttributes.hh>
+#include <G4ProcessManager.hh>
+#include <G4VTrackingManager.hh>
+#include <G4DecayTable.hh>
+#include <G4ParticleTable.hh>
+#include <G4ParticlePropertyTable.hh>
+#include <G4PrimaryParticle.hh>
+#include <G4DecayProducts.hh>
+#include <G4NavigationHistory.hh>
+#include <G4ReferenceCountedHandle.hh>
+#include <G4DensityEffectData.hh>
+#include <G4DensityEffectCalculator.hh>
+#include <G4Run.hh>
+#include <G4Event.hh>
+#include <G4VAuxiliaryTrackInformation.hh>
 #include <G4VProcess.hh>
 
 #include "typecast.hh"
@@ -11,39 +38,107 @@ namespace py = pybind11;
 
 void export_G4Track(py::module &m)
 {
-   py::class_<G4Track>(m, "G4Track", "track class")
-      .def(py::init<>())
-      .def(py::init<G4DynamicParticle *, G4double, const G4ThreeVector &>())
-      .def(py::init<const G4Track &>())
+   py::class_<G4Track>(m, "G4Track")
 
-      .def("GetTrackID", &G4Track::GetTrackID)
-      .def("GetParentID", &G4Track::GetParentID)
-      .def("GetDynamicParticle", &G4Track::GetDynamicParticle, py::return_value_policy::reference_internal)
-      .def("GetDefinition", &G4Track::GetDefinition, py::return_value_policy::reference_internal)
-      .def("GetPosition", &G4Track::GetPosition)
-      .def("GetGlobalTime", &G4Track::GetGlobalTime)
-      .def("GetLocalTime", &G4Track::GetLocalTime)
-      .def("GetProperTime", &G4Track::GetProperTime)
-      .def("GetVolume", &G4Track::GetVolume, py::return_value_policy::reference)
-      .def("GetMaterial", &G4Track::GetMaterial, py::return_value_policy::reference)
-      .def("GetTouchable", &G4Track::GetTouchable, py::return_value_policy::reference)
-      .def("GetKineticEnergy", &G4Track::GetKineticEnergy)
-      .def("GetTotalEnergy", &G4Track::GetTotalEnergy)
-      .def("GetMomentumDirection", &G4Track::GetMomentumDirection)
-      .def("GetMomentum", &G4Track::GetMomentum)
-      .def("GetVelocity", &G4Track::GetVelocity)
-      .def("GetPolarization", &G4Track::GetPolarization)
-      .def("GetTrackStatus", &G4Track::GetTrackStatus)
-      .def("GetTrackLength", &G4Track::GetTrackLength)
-      .def("GetStep", &G4Track::GetStep, py::return_value_policy::reference)
-      .def("GetCurrentStepNumber", &G4Track::GetCurrentStepNumber)
-      .def("GetStepLength", &G4Track::GetStepLength)
-      .def("GetVertexPosition", &G4Track::GetVertexPosition)
-      .def("GetVertexMomentumDirection", &G4Track::GetVertexMomentumDirection)
-      .def("GetVertexKineticEnergy", &G4Track::GetVertexKineticEnergy)
-      .def("GetLogicalVolumeAtVertex", &G4Track::GetLogicalVolumeAtVertex, py::return_value_policy::reference)
+      .def(py::init<>())
+      .def(py::init<G4DynamicParticle *, G4double, const G4ThreeVector &>(), py::arg("apValueDynamicParticle"),
+           py::arg("aValueTime"), py::arg("aValuePosition"))
+
+      .def("__copy__", [](const G4Track &self) { return new G4Track(self); })
+      .def("__deepcopy__", [](const G4Track &self, py::dict) { return new G4Track(self); })
+      .def("AddTrackLength", &G4Track::AddTrackLength, py::arg("aValue"))
+      .def("CalculateVelocity", &G4Track::CalculateVelocity)
+      .def("CalculateVelocityForOpticalPhoton", &G4Track::CalculateVelocityForOpticalPhoton)
+      .def("CopyTrackInfo", &G4Track::CopyTrackInfo)
+      .def("GetAuxiliaryTrackInformation", &G4Track::GetAuxiliaryTrackInformation, py::arg("id"),
+           py::return_value_policy::reference)
+
+      .def("GetAuxiliaryTrackInformationMap", &G4Track::GetAuxiliaryTrackInformationMap,
+           py::return_value_policy::reference)
+
+      .def("GetCreatorModelID", &G4Track::GetCreatorModelID)
+      .def("GetCreatorModelIndex", &G4Track::GetCreatorModelIndex)
+      .def("GetCreatorModelName", &G4Track::GetCreatorModelName)
       .def("GetCreatorProcess", &G4Track::GetCreatorProcess, py::return_value_policy::reference)
+      .def("GetCurrentStepNumber", &G4Track::GetCurrentStepNumber)
+      .def("GetDefinition", &G4Track::GetDefinition, py::return_value_policy::reference)
+      .def("GetDynamicParticle", &G4Track::GetDynamicParticle, py::return_value_policy::reference)
+      .def("GetGlobalTime", &G4Track::GetGlobalTime)
+      .def("GetKineticEnergy", &G4Track::GetKineticEnergy)
+      .def("GetLocalTime", &G4Track::GetLocalTime)
+      .def("GetLogicalVolumeAtVertex", &G4Track::GetLogicalVolumeAtVertex, py::return_value_policy::reference)
+      .def("GetMaterial", &G4Track::GetMaterial, py::return_value_policy::reference)
+      .def("GetMaterialCutsCouple", &G4Track::GetMaterialCutsCouple, py::return_value_policy::reference)
+      .def("GetMomentum", &G4Track::GetMomentum)
+      .def("GetMomentumDirection", &G4Track::GetMomentumDirection)
+      .def("GetNextMaterial", &G4Track::GetNextMaterial, py::return_value_policy::reference)
+      .def("GetNextMaterialCutsCouple", &G4Track::GetNextMaterialCutsCouple, py::return_value_policy::reference)
+      .def("GetNextTouchable", &G4Track::GetNextTouchable, py::return_value_policy::reference)
+      .def("GetNextTouchableHandle", &G4Track::GetNextTouchableHandle)
+      .def("GetNextVolume", &G4Track::GetNextVolume, py::return_value_policy::reference)
+      .def("GetOriginTouchable", &G4Track::GetOriginTouchable, py::return_value_policy::reference)
+      .def("GetOriginTouchableHandle", &G4Track::GetOriginTouchableHandle)
+      .def("GetParentID", &G4Track::GetParentID)
+      .def("GetParticleDefinition", &G4Track::GetParticleDefinition, py::return_value_policy::reference)
+      .def("GetPolarization", &G4Track::GetPolarization)
+      .def("GetPosition", &G4Track::GetPosition)
+      .def("GetProperTime", &G4Track::GetProperTime)
+      .def("GetStep", &G4Track::GetStep, py::return_value_policy::reference)
+      .def("GetStepLength", &G4Track::GetStepLength)
+      .def("GetTotalEnergy", &G4Track::GetTotalEnergy)
+      .def("GetTouchable", &G4Track::GetTouchable, py::return_value_policy::reference)
+      .def("GetTouchableHandle", &G4Track::GetTouchableHandle)
+      .def("GetTrackID", &G4Track::GetTrackID)
+      .def("GetTrackLength", &G4Track::GetTrackLength)
+      .def("GetTrackStatus", &G4Track::GetTrackStatus)
+      .def("GetUserInformation", &G4Track::GetUserInformation, py::return_value_policy::reference)
+      .def("GetVelocity", &G4Track::GetVelocity)
+      .def("GetVertexKineticEnergy", &G4Track::GetVertexKineticEnergy)
+      .def("GetVertexMomentumDirection", &G4Track::GetVertexMomentumDirection)
+      .def("GetVertexPosition", &G4Track::GetVertexPosition)
+      .def("GetVolume", &G4Track::GetVolume, py::return_value_policy::reference)
       .def("GetWeight", &G4Track::GetWeight)
-      .def("SetWeight", &G4Track::SetWeight)
-      .def("GetDefinition", &G4Track::GetDefinition, py::return_value_policy::reference);
+      .def("IncrementCurrentStepNumber", &G4Track::IncrementCurrentStepNumber)
+      .def("IsBelowThreshold", &G4Track::IsBelowThreshold)
+      .def("IsGoodForTracking", &G4Track::IsGoodForTracking)
+      .def("RemoveAuxiliaryTrackInformation", py::overload_cast<G4int>(&G4Track::RemoveAuxiliaryTrackInformation),
+           py::arg("id"))
+
+      .def(
+         "RemoveAuxiliaryTrackInformation",
+         [](G4Track &self, G4String &name) {
+            self.RemoveAuxiliaryTrackInformation(name);
+            return name;
+         },
+         py::arg("name"))
+
+      .def("SetAuxiliaryTrackInformation", &G4Track::SetAuxiliaryTrackInformation, py::arg("id"), py::arg("info"))
+      .def("SetBelowThresholdFlag", &G4Track::SetBelowThresholdFlag, py::arg("value") = true)
+      .def("SetCreatorModelID", &G4Track::SetCreatorModelID, py::arg("id"))
+      .def("SetCreatorProcess", &G4Track::SetCreatorProcess, py::arg("aValue"))
+      .def("SetGlobalTime", &G4Track::SetGlobalTime, py::arg("aValue"))
+      .def("SetGoodForTrackingFlag", &G4Track::SetGoodForTrackingFlag, py::arg("value") = true)
+      .def("SetKineticEnergy", &G4Track::SetKineticEnergy, py::arg("aValue"))
+      .def("SetLocalTime", &G4Track::SetLocalTime, py::arg("aValue"))
+      .def("SetLogicalVolumeAtVertex", &G4Track::SetLogicalVolumeAtVertex)
+      .def("SetMomentumDirection", &G4Track::SetMomentumDirection, py::arg("aValue"))
+      .def("SetNextTouchableHandle", &G4Track::SetNextTouchableHandle, py::arg("apValue"))
+      .def("SetOriginTouchableHandle", &G4Track::SetOriginTouchableHandle, py::arg("apValue"))
+      .def("SetParentID", &G4Track::SetParentID, py::arg("aValue"))
+      .def("SetPolarization", &G4Track::SetPolarization, py::arg("aValue"))
+      .def("SetPosition", &G4Track::SetPosition, py::arg("aValue"))
+      .def("SetProperTime", &G4Track::SetProperTime, py::arg("aValue"))
+      .def("SetStep", &G4Track::SetStep, py::arg("aValue"))
+      .def("SetStepLength", &G4Track::SetStepLength, py::arg("value"))
+      .def("SetTouchableHandle", &G4Track::SetTouchableHandle, py::arg("apValue"))
+      .def("SetTrackID", &G4Track::SetTrackID, py::arg("aValue"))
+      .def("SetTrackStatus", &G4Track::SetTrackStatus, py::arg("aTrackStatus"))
+      .def("SetUserInformation", &G4Track::SetUserInformation, py::arg("aValue"))
+      .def("SetVelocity", &G4Track::SetVelocity, py::arg("val"))
+      .def("SetVertexKineticEnergy", &G4Track::SetVertexKineticEnergy, py::arg("aValue"))
+      .def("SetVertexMomentumDirection", &G4Track::SetVertexMomentumDirection, py::arg("aValue"))
+      .def("SetVertexPosition", &G4Track::SetVertexPosition, py::arg("aValue"))
+      .def("SetWeight", &G4Track::SetWeight, py::arg("aValue"))
+      .def("UseGivenVelocity", py::overload_cast<>(&G4Track::UseGivenVelocity, py::const_))
+      .def("UseGivenVelocity", py::overload_cast<G4bool>(&G4Track::UseGivenVelocity), py::arg("val"));
 }
